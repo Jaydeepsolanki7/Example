27248b9414de967a86760bee04ff11945fd2803e
Jaydeepsolanki76 <jay.solanki7694@gmail.com>
2023-09-26 12:02:20 +0530

    Add some changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@
 
 class HomeController < ApplicationController
   def index
-    @uploads = Upload.all.paginate(page: params[:page], per_page: 4)
+    if !current_user == nil?
+      @uploads = Upload.all.paginate(page: params[:page], per_page: 4)
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
